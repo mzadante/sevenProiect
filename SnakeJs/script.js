a8b1a4be8de6f44f4c6c25a459a067051d4e692c
Dante Martinez <mzadantemartinez@gmail.com>
2024-05-10 13:14:02 -0300

    tablero creado mas serpiente pintada

--- SnakeJs/script.js
+++ SnakeJs/script.js
@@ -23,4 +23,59 @@ let snake;
 let score;
 let directions;
 let boardSquares;
+let emptySquares;
+let moveInterval;
 
+const drawSnake = () => {
+    snake.forEach (square => drawSquare(square, 'snakeSquare'));
+}
+
+// Rellena cada cuadrado del tablero
+//@params
+// square: posicion del cuadrado
+// type: tipo de cuadrado (emptySquare, snakeSquare, foodSquare)
+const drawSquare = (square, type) => {
+    const [row, column] = square.split('');
+    boardSquares[row][column] = typesSquares[type];
+    const squareElement = document.getElementById(square);
+    squareElement.setAttribute('class', `square ${type}`);
+    if(type === 'emptySquare') {
+        emptySquares.push(square);
+    }else{
+        if(emptySquares.indexOf(square) !== -1) {
+            emptySquares.splice(emptySquares.indexOf(square), 1);
+        }
+    }
+}
+
+const createBoard = () => {
+    boardSquares.forEach((row, rowIndex) => {
+        row.forEach((column, columnIndex) => {
+            const squareValue = `${rowIndex}${columnIndex}`;
+            const squareElement = document.createElement('div');
+            squareElement.setAttribute('class', 'square emptySquare');
+            squareElement.setAttribute('id', squareValue);
+            tableroGame.appendChild(squareElement);
+            emptySquares.push(squareValue);
+        });
+    });
+}
+
+const setGame = () => {
+    snake = ['00', '01', '02', '03'];
+    score = snake.length;
+    directions ='ArrowRight';
+    boardSquares = Array.from(Array(tableroSize), () => new Array(tableroSize).fill(typesSquares.emptySquare));
+    console.log(boardSquares);
+    tableroGame.innerHTML = '';
+    emptySquares = [];
+    createBoard();
+}
+
+const startGame = () => {
+    setGame();
+    gameOverSign.style.display = 'none';
+    startButton.style.display = 'none';
+    drawSnake();
+}
+ startButton.addEventListener('click', startGame);
